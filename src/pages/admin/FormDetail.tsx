7a5202ac3dede4d110f92b73ea3e568d3fb8d10d
Nurdin <mrsunnysummer@gmail.com>
2024-10-05 19:37:24 +0700

    chore: add xlsx

--- src/pages/admin/FormDetail.tsx
+++ src/pages/admin/FormDetail.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable prefer-const */
 import DashboardLayout from "@/components/layout/DashboardLayout"
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
