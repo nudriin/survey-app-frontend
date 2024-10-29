7d329f4847e82e036add39b8df098553652ef9a4
Nurdin <mrsunnysummer@gmail.com>
2024-10-30 05:40:02 +0700

    feat: create nrr bar chart

--- src/components/skm/SkmResultTable.tsx
+++ src/components/skm/SkmResultTable.tsx
@@ -1,5 +1,19 @@
 import { ResponsesWithQuestionResponse } from "@/model/SkmModel"
 import { useCallback, useEffect, useState } from "react"
+import {
+    ChartConfig,
+    ChartContainer,
+    ChartTooltip,
+    ChartTooltipContent,
+} from "../ui/chart"
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "../ui/card"
+import { Bar, BarChart, CartesianGrid, LabelList, XAxis } from "recharts"
 
 export default function SkmResultTable() {
     const [responsesQuestion, setResponsesQuestion] = useState<
@@ -33,8 +47,16 @@ export default function SkmResultTable() {
 
     return (
         <>
+            <div className="flex justify-between mb-4">
+                <h1 className="text-3xl font-semibold text-left">
+                    Tabel Nilai Rata-Rata
+                </h1>
+            </div>
+            <div className="grid grid-cols-2 mb-10">
+                <NrrStatusTable responsesQuestion={responsesQuestion} />
+                <NrrBarChart responsesQuestion={responsesQuestion} />
+            </div>
             <ResultTable responsesQuestion={responsesQuestion} />
-            <NrrStatusTable responsesQuestion={responsesQuestion} />
         </>
     )
 }
@@ -47,7 +69,9 @@ export function ResultTable({
     return (
         <>
             <div className="flex justify-between mb-4">
-                <h1 className="text-3xl font-semibold text-left">Tabel NRR</h1>
+                <h1 className="text-3xl font-semibold text-left">
+                    Tabel Nilai Rata-Rata
+                </h1>
             </div>
             <div className="overflow-x-auto text-center">
                 <table className="w-full">
@@ -136,7 +160,7 @@ export function ResultTable({
                                 </td>
                             ))}
                         </tr>
-                        <tr className="bg-primary text-secondary">
+                        <tr className="bg-muted-foreground text-secondary">
                             <td className="p-1 border-2 border-primary border-r-secondary font-semibold text-left">
                                 IKM Unit Pelayanan
                             </td>
@@ -177,77 +201,145 @@ export function NrrStatusTable({
     responsesQuestion: ResponsesWithQuestionResponse[]
 }) {
     return (
-        <div className="overflow-x-auto text-left">
-            <table>
-                <thead>
-                    <tr>
-                        <th className="p-1 border-2 bg-purples text-white border-primary">
-                            No
-                        </th>
-                        <th className="p-1 border-2 bg-purples text-white border-primary">
-                            Unsur Pelayanan
-                        </th>
-                        <th className="p-1 border-2 bg-purples text-white border-primary">
-                            NRR
-                        </th>
-                        <th className="p-1 border-2 bg-purples text-white border-primary">
-                            Keterangan
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {responsesQuestion.map((value, index) => (
+        <div>
+            <div className="overflow-x-auto text-left">
+                <table>
+                    <thead>
                         <tr>
-                            <td className="p-1 border-2 border-primary px-2 py-1">
-                                U{index + 1}
-                            </td>
-                            <td
-                                key={value.id}
-                                className="p-1 border-2 border-primary px-2 py-1"
-                            >
-                                {value.acronim}
-                            </td>
-                            <td className="p-1 border-2 border-primary px-2 py-1">
-                                {(
-                                    value.responses.reduce(
-                                        (total, opt) =>
-                                            total + opt.select_option,
-                                        0
-                                    ) / value.responses.length
-                                ).toFixed(3)}
-                            </td>
-                            <td className="p-1 border-2 border-primary px-2 py-1">
-                                {(() => {
-                                    const a = (
+                            <th className="p-1 border-2 bg-purples text-white border-primary">
+                                No
+                            </th>
+                            <th className="p-1 border-2 bg-purples text-white border-primary">
+                                Unsur Pelayanan
+                            </th>
+                            <th className="p-1 border-2 bg-purples text-white border-primary">
+                                NRR
+                            </th>
+                            <th className="p-1 border-2 bg-purples text-white border-primary">
+                                Keterangan
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {responsesQuestion.map((value, index) => (
+                            <tr>
+                                <td className="p-1 border-2 border-primary px-2 py-1">
+                                    U{index + 1}
+                                </td>
+                                <td
+                                    key={value.id}
+                                    className="p-1 border-2 border-primary px-2 py-1"
+                                >
+                                    {value.acronim}
+                                </td>
+                                <td className="p-1 border-2 border-primary px-2 py-1">
+                                    {(
                                         value.responses.reduce(
                                             (total, opt) =>
                                                 total + opt.select_option,
                                             0
                                         ) / value.responses.length
-                                    ).toFixed(3)
-
-                                    const floatA = parseFloat(a)
-                                    if (floatA >= 1.0 && floatA <= 2.5996) {
-                                        return "Tidak Baik"
-                                    } else if (
-                                        floatA >= 2.6 &&
-                                        floatA <= 3.064
-                                    ) {
-                                        return "Kurang Baik"
-                                    } else if (
-                                        floatA >= 3.0644 &&
-                                        floatA <= 3.532
-                                    ) {
-                                        return "Baik"
-                                    } else {
-                                        return "Sangat Baik"
-                                    }
-                                })()}
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+                                    ).toFixed(3)}
+                                </td>
+                                <td className="p-1 border-2 border-primary px-2 py-1">
+                                    {(() => {
+                                        const a = (
+                                            value.responses.reduce(
+                                                (total, opt) =>
+                                                    total + opt.select_option,
+                                                0
+                                            ) / value.responses.length
+                                        ).toFixed(3)
+                                        const floatA = parseFloat(a)
+                                        if (floatA >= 1.0 && floatA <= 2.5996) {
+                                            return "Tidak Baik"
+                                        } else if (
+                                            floatA >= 2.6 &&
+                                            floatA <= 3.064
+                                        ) {
+                                            return "Kurang Baik"
+                                        } else if (
+                                            floatA >= 3.0644 &&
+                                            floatA <= 3.532
+                                        ) {
+                                            return "Baik"
+                                        } else {
+                                            return "Sangat Baik"
+                                        }
+                                    })()}
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
+    )
+}
+
+export function NrrBarChart({
+    responsesQuestion,
+}: {
+    responsesQuestion: ResponsesWithQuestionResponse[]
+}) {
+    const color = "#5831ee"
+    const chartData = responsesQuestion.map((item, index) => ({
+        label: `U${index + 1}`, // Label sesuai index + 1
+        value: (
+            item.responses.reduce(
+                (total, opt) => total + opt.select_option,
+                0
+            ) / item.responses.length
+        ).toFixed(3), // Nilai rata-rata
+    }))
+
+    const chartConfig = {
+        value: {
+            label: "Rata-rata Penilaian",
+            color: color,
+        },
+    } satisfies ChartConfig
+
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle>Bar Chart - Rata-rata Penilaian</CardTitle>
+                <CardDescription>Penilaian per Pertanyaan</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <ChartContainer config={chartConfig}>
+                    <BarChart
+                        accessibilityLayer
+                        data={chartData}
+                        margin={{ top: 20 }}
+                    >
+                        <CartesianGrid vertical={false} />
+                        <XAxis
+                            dataKey="label"
+                            tickLine={false}
+                            tickMargin={10}
+                            axisLine={false}
+                        />
+                        <ChartTooltip
+                            cursor={false}
+                            content={<ChartTooltipContent hideLabel />}
+                        />
+                        <Bar
+                            dataKey="value"
+                            fill={color}
+                            radius={3}
+                            barSize={30}
+                        >
+                            <LabelList
+                                position="top"
+                                offset={12}
+                                className="fill-foreground"
+                                fontSize={12}
+                            />
+                        </Bar>
+                    </BarChart>
+                </ChartContainer>
+            </CardContent>
+        </Card>
     )
 }
