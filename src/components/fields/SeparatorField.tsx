698a616dcaa3977a4655d43b1d82b1134d3a1ecb
Nurdin <mrsunnysummer@gmail.com>
2024-11-08 23:26:45 +0700

    fix: build error

--- src/components/fields/SeparatorField.tsx
+++ src/components/fields/SeparatorField.tsx
@@ -1,25 +1,11 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable react-refresh/only-export-components */
-import { ElementsType, FormElement, FormElementInstance } from '../FormElement';
-import { RiSeparator } from 'react-icons/ri';
-import { Label } from '../ui/label';
-import { Input } from '../ui/input';
-import { z } from 'zod';
-import { useForm } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useEffect } from 'react';
-import useDesigner from '@/hooks/useDesigner';
-import {
-    Form,
-    FormControl,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
-} from '../ui/form';
-import { Separator } from '../ui/separator';
+import { ElementsType, FormElement } from "../FormElement"
+import { RiSeparator } from "react-icons/ri"
+import { Label } from "../ui/label"
+import { Separator } from "../ui/separator"
 
-const type: ElementsType = 'SeparatorField';
+const type: ElementsType = "SeparatorField"
 
 export const SeparatorFieldFormElement: FormElement = {
     type,
@@ -31,42 +17,30 @@ export const SeparatorFieldFormElement: FormElement = {
 
     designerBtn: {
         icon: RiSeparator,
-        label: 'Separator Field',
+        label: "Separator Field",
     },
 
     designerComponent: DesignerComponent,
     formComponent: FormComponent,
     propertiesComponent: PropertiesComponent,
     validate: () => true,
-};
+}
 
-function DesignerComponent({
-    elementInstance,
-}: {
-    elementInstance: FormElementInstance;
-}) {
+function DesignerComponent() {
     return (
-        <div className="flex flex-col gap-2 w-full items-start">
+        <div className="flex flex-col items-start w-full gap-2">
             <Label className="font-semibold text-muted-foreground">
                 Separator
             </Label>
             <Separator />
         </div>
-    );
+    )
 }
 
-function PropertiesComponent({
-    elementInstance,
-}: {
-    elementInstance: FormElementInstance;
-}) {
-    return <p>No properties</p>;
+function PropertiesComponent() {
+    return <p>No properties</p>
 }
 
-function FormComponent({
-    elementInstance,
-}: {
-    elementInstance: FormElementInstance;
-}) {
-    return <Separator />;
+function FormComponent() {
+    return <Separator />
 }
